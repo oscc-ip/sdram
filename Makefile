2169d0f9fe5642263f1c9fc0a5b3642946c49485
myyerrol <myyerrol@126.com>
2023-09-07 17:48:09 +0800

    feat: add init file

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@
 
 sim:
 	cd models && \
-	ncverilog +access+r +define+clk_133 W989DxDB.nc.vp ../sim/sdram_test.v
+	ncverilog +access+r +define+clk_133+x16 W989DxDB.nc.vp ../sim/sdram_test.v
 clean:
 	find -name "*.log" -o -name "*.history" -o -name "*.key" | xargs rm -f
 	find -name "INCA_libs" | xargs rm -rf
--- sim/sdram_test.v
+++ sim/sdram_test.v
@@ -6,18 +6,19 @@ module sdram_test;
 
 reg                     clk;
 reg                     cke;
-reg [ADDR_BITS - 1 : 0] addr;
-reg   [BA_BITS - 1 : 0] ba;
 reg                     cs_n;
 reg                     ras_n;
 reg                     cas_n;
 reg                     we_n;
+reg [ADDR_BITS - 1 : 0] addr;
+reg   [BA_BITS - 1 : 0] ba;
 reg   [DQ_BITS - 1 : 0] dq;
 reg   [DM_BITS - 1 : 0] dqm;
 
 wire  [DQ_BITS - 1 : 0] DQ = dq;
 
 parameter hi_z = {DQ_BITS{1'bz}};
+parameter t100us = 1000000000;
 
 W989DxDB sdram(clk, cke, addr, ba, cs_n, ras_n, cas_n, we_n, DQ, dqm);
 
@@ -30,6 +31,64 @@ end
 
 always #(tCK / 2) clk = ~clk;
 
+task NOP;
+    input [DM_BITS - 1 : 0] dqm_in;
+    input [DQ_BITS - 1 : 0] dq_in;
+    begin
+        cke   = 1;
+        cs_n  = 0;
+        ras_n = 1;
+        cas_n = 1;
+        we_n  = 1;
+        dqm   = dqm_in;
+        dq    = dq_in;
+    end
+endtask
+
+task PERA;
+    input [DM_BITS - 1 : 0] dqm_in;
+    input [DQ_BITS - 1 : 0] dq_in;
+    begin
+        cke   = 1;
+        cs_n  = 0;
+        ras_n = 0;
+        cas_n = 1;
+        we_n  = 0;
+        addr  = 1024; // A10 = 1
+        ba    = 0;
+        dqm   = dqm_in;
+        dq    = dq_in;
+    end
+endtask
+
+task AREF;
+    begin
+        cke   = 1;
+        cs_n  = 0;
+        ras_n = 0;
+        cas_n = 0;
+        we_n  = 1;
+        dqm   = 0;
+        dq    = hi_z;
+    end
+endtask
+
+task LMR;
+    input [ADDR_BITS - 1 : 0] op_code;
+    begin
+        cke   = 1;
+        cs_n  = 0;
+        ras_n = 0;
+        cas_n = 0;
+        we_n  = 0;
+        addr  = op_code;
+        ba    = 0;
+        dqm   = 0;
+        dq    = hi_z;
+    end
+endtask
+
+
 task active;
     input [ADDR_BITS - 1 : 0] bank;
     input  [ROW_BITS - 1 : 0] row;
@@ -40,26 +99,14 @@ task active;
         ras_n = 0;
         cas_n = 1;
         we_n  = 1;
-        dqm   = 0;
         ba    = bank;
         addr  = row;
+        dqm   = 0;
         dq    = dq_in;
     end
 endtask
 
-task auto_refresh;
-    begin
-        cke   = 1;
-        cs_n  = 0;
-        ras_n = 0;
-        cas_n = 0;
-        we_n  = 1;
-        dqm   = 0;
-        //ba    = 0;
-        //addr  = 0;
-        dq    = hi_z;
-    end
-endtask
+
 
 task burst_term;
     input [DQ_BITS - 1 : 0] dq_in;
@@ -76,36 +123,7 @@ task burst_term;
     end
 endtask
 
-task load_mode_reg;
-    input [ADDR_BITS - 1 : 0] op_code;
-    begin
-        cke   = 1;
-        cs_n  = 0;
-        ras_n = 0;
-        cas_n = 0;
-        we_n  = 0;
-        dqm   = 0;
-        ba    = 0;
-        addr  = op_code;
-        dq    = hi_z;
-    end
-endtask
 
-task nop;
-    input [DM_BITS - 1 : 0] dqm_in;
-    input [DQ_BITS - 1 : 0] dq_in;
-    begin
-        cke   = 1;
-        cs_n  = 0;
-        ras_n = 1;
-        cas_n = 1;
-        we_n  = 1;
-        dqm   = dqm_in;
-        //ba    = 0;
-        //addr  = 0;
-        dq    = dq_in;
-    end
-endtask
 
 task precharge_bank_0;
     input [DM_BITS - 1 : 0] dqm_in;
@@ -171,21 +189,7 @@ task precharge_bank_3;
     end
 endtask
 
-task precharge_all_bank;
-    input [DM_BITS - 1 : 0] dqm_in;
-    input [DQ_BITS - 1 : 0] dq_in;
-    begin
-        cke   = 1;
-        cs_n  = 0;
-        ras_n = 0;
-        cas_n = 1;
-        we_n  = 0;
-        dqm   = dqm_in;
-        ba    = 0;
-        addr  = 1024;            // A10 = 1
-        dq    = dq_in;
-    end
-endtask
+
 
 task read;
     input   [BA_BITS - 1 : 0] bank;
@@ -226,128 +230,118 @@ endtask
 initial begin
     begin
         // Initialize
-        #tCK; nop    (0, hi_z);                 // Nop
-        #tCK; nop    (0, hi_z);                 // Nop
-        #tCK; nop    (0, hi_z);                 // Nop
-        #tCK; nop    (0, hi_z);                 // Nop
-        #tCK; nop    (0, hi_z);                 // Nop
-        #tCK; nop    (0, hi_z);                 // Nop
-        #tCK; nop    (0, hi_z);                 // Nop
-        #tCK; nop    (0, hi_z);                 // Nop
-        #tCK; nop    (0, hi_z);                 // Nop
-        #tCK; nop    (0, hi_z);                 // Nop
-        #tCK; precharge_all_bank(0, hi_z);      // Precharge ALL Bank
-        #tCK; nop    (0, hi_z);                 // Nop
-        #tCK; nop    (0, hi_z);                 // Nop
-        #tCK; nop    (0, hi_z);                 // Nop
-        #tCK; nop    (0, hi_z);                 // Nop
-        #tCK; auto_refresh;                     // Auto Refresh
-        #tCK; nop    (0, hi_z);                 // Nop
-        #tCK; nop    (0, hi_z);                 // Nop
-        #tCK; nop    (0, hi_z);                 // Nop
-        #tCK; nop    (0, hi_z);                 // Nop
-        #tCK; nop    (0, hi_z);                 // Nop
-        #tCK; nop    (0, hi_z);                 // Nop
-        #tCK; nop    (0, hi_z);                 // Nop
-        #tCK; nop    (0, hi_z);                 // Nop
-        #tCK; nop    (0, hi_z);                 // Nop
-        #tCK; auto_refresh;                     // Auto Refresh
-        #tCK; nop    (0, hi_z);                 // Nop
-        #tCK; nop    (0, hi_z);                 // Nop
-        #tCK; nop    (0, hi_z);                 // Nop
-        #tCK; nop    (0, hi_z);                 // Nop
-        #tCK; nop    (0, hi_z);                 // Nop
-        #tCK; nop    (0, hi_z);                 // Nop
-        #tCK; nop    (0, hi_z);                 // Nop
-        #tCK; nop    (0, hi_z);                 // Nop
-        #tCK; nop    (0, hi_z);                 // Nop
-        #tCK; load_mode_reg (50);               // Load Mode: Lat = 3, BL = 4, Seq
-        #tCK; nop    (0, hi_z);                 // Nop
+        #t100us;
+        #tCK; NOP    (0, hi_z);                 // Nop
+        #tCK; PERA(0, hi_z);                    // Precharge ALL Bank
+        #tCK; NOP    (0, hi_z);                 // Nop
+        #tCK; NOP    (0, hi_z);                 // Nop
+        #tCK; AREF;                             // Auto Refresh
+        #tCK; NOP    (0, hi_z);                 // Nop
+        #tCK; NOP    (0, hi_z);                 // Nop
+        #tCK; NOP    (0, hi_z);                 // Nop
+        #tCK; NOP    (0, hi_z);                 // Nop
+        #tCK; NOP    (0, hi_z);                 // Nop
+        #tCK; NOP    (0, hi_z);                 // Nop
+        #tCK; NOP    (0, hi_z);                 // Nop
+        #tCK; NOP    (0, hi_z);                 // Nop
+        #tCK; NOP    (0, hi_z);                 // Nop
+        #tCK; AREF;                             // Auto Refresh
+        #tCK; NOP    (0, hi_z);                 // Nop
+        #tCK; NOP    (0, hi_z);                 // Nop
+        #tCK; NOP    (0, hi_z);                 // Nop
+        #tCK; NOP    (0, hi_z);                 // Nop
+        #tCK; NOP    (0, hi_z);                 // Nop
+        #tCK; NOP    (0, hi_z);                 // Nop
+        #tCK; NOP    (0, hi_z);                 // Nop
+        #tCK; NOP    (0, hi_z);                 // Nop
+        #tCK; NOP    (0, hi_z);                 // Nop
+        #tCK; LMR (50);                         // Load Mode: Lat = 3, BL = 4, Seq
+        #tCK; NOP    (0, hi_z);                 // Nop
 
         // Write with auto precharge to bank 0 (non-interrupt)
         #tCK; active (0, 0, hi_z);              // Active: Bank = 0, Row = 0
-        #tCK; nop    (0, hi_z);                 // Nop
-        #tCK; nop    (0, hi_z);                 // Nop
+        #tCK; NOP    (0, hi_z);                 // Nop
+        #tCK; NOP    (0, hi_z);                 // Nop
         #tCK; write  (0, 1024, $random, 0);     // Write : Bank = 0, Col = 0, Dqm = 0, Auto Precharge
-        #tCK; nop    (0, $random);              // Nop
-        #tCK; nop    (0, $random);              // Nop
-        #tCK; nop    (0, $random);              // Nop
-        #tCK; nop    (0, hi_z);                 // Nop
+        #tCK; NOP    (0, $random);              // Nop
+        #tCK; NOP    (0, $random);              // Nop
+        #tCK; NOP    (0, $random);              // Nop
+        #tCK; NOP    (0, hi_z);                 // Nop
 
-        // Write with auto precharge to bank 1 (non-interrupt)
-        #tCK; active (1, 0, hi_z);              // Active: Bank = 1, Row = 0
-        #tCK; nop    (0, hi_z);                 // Nop
-        #tCK; nop    (0, hi_z);                 // Nop
-        #tCK; write  (1, 1024, $random, 0);     // Write : Bank = 1, Col = 0, Dqm = 0, Auto precharge
-        #tCK; nop    (0, $random);              // Nop
-        #tCK; nop    (0, $random);              // Nop
-        #tCK; nop    (0, $random);              // Nop
-        #tCK; nop    (0, hi_z);                 // Nop
+        // // Write with auto precharge to bank 1 (non-interrupt)
+        // #tCK; active (1, 0, hi_z);              // Active: Bank = 1, Row = 0
+        // #tCK; NOP    (0, hi_z);                 // Nop
+        // #tCK; NOP    (0, hi_z);                 // Nop
+        // #tCK; write  (1, 1024, $random, 0);     // Write : Bank = 1, Col = 0, Dqm = 0, Auto precharge
+        // #tCK; NOP    (0, $random);              // Nop
+        // #tCK; NOP    (0, $random);              // Nop
+        // #tCK; NOP    (0, $random);              // Nop
+        // #tCK; NOP    (0, hi_z);                 // Nop
 
-        // Write with auto precharge to bank 2 (non-interrupt)
-        #tCK; active (2, 0, hi_z);              // Active: Bank = 2, Row = 0
-        #tCK; nop    (0, hi_z);                 // Nop
-        #tCK; nop    (0, hi_z);                 // Nop
-        #tCK; write  (2, 1024, $random, 0);     // Write : Bank = 2, Col = 0, Dqm = 0, Auto Precharge
-        #tCK; nop    (0, $random);              // Nop
-        #tCK; nop    (0, $random);              // Nop
-        #tCK; nop    (0, $random);              // Nop
-        #tCK; nop    (0, hi_z);                 // Nop
+        // // Write with auto precharge to bank 2 (non-interrupt)
+        // #tCK; active (2, 0, hi_z);              // Active: Bank = 2, Row = 0
+        // #tCK; NOP    (0, hi_z);                 // Nop
+        // #tCK; NOP    (0, hi_z);                 // Nop
+        // #tCK; write  (2, 1024, $random, 0);     // Write : Bank = 2, Col = 0, Dqm = 0, Auto Precharge
+        // #tCK; NOP    (0, $random);              // Nop
+        // #tCK; NOP    (0, $random);              // Nop
+        // #tCK; NOP    (0, $random);              // Nop
+        // #tCK; NOP    (0, hi_z);                 // Nop
 
-        // Write with auto precharge to bank 3 (non-interrupt)
-        #tCK; active (3, 0, hi_z);              // Active: Bank = 3, Row = 0
-        #tCK; nop    (0, hi_z);                 // Nop
-        #tCK; nop    (0, hi_z);                 // Nop
-        #tCK; write  (3, 1024, $random, 0);     // Write : Bank = 3, Col = 0, Dqm = 0, Auto precharge
-        #tCK; nop    (0, $random);              // Nop
-        #tCK; nop    (0, $random);              // Nop
-        #tCK; nop    (0, $random);              // Nop
-        #tCK; nop    (0, hi_z);                 // Nop
+        // // Write with auto precharge to bank 3 (non-interrupt)
+        // #tCK; active (3, 0, hi_z);              // Active: Bank = 3, Row = 0
+        // #tCK; NOP    (0, hi_z);                 // Nop
+        // #tCK; NOP    (0, hi_z);                 // Nop
+        // #tCK; write  (3, 1024, $random, 0);     // Write : Bank = 3, Col = 0, Dqm = 0, Auto precharge
+        // #tCK; NOP    (0, $random);              // Nop
+        // #tCK; NOP    (0, $random);              // Nop
+        // #tCK; NOP    (0, $random);              // Nop
+        // #tCK; NOP    (0, hi_z);                 // Nop
 
-        // Read with auto precharge to bank 0 (non-interrupt)
-        #tCK; active (0, 0, hi_z);              // Active: Bank = 0, Row = 0
-        #tCK; nop    (0, hi_z);                 // Nop
-        #tCK; nop    (0, hi_z);                 // Nop
-        #tCK; read   (0, 1024, hi_z, 0);        // Read  : Bank = 0, Col = 0, Dqm = 0, Auto precharge
-        #tCK; nop    (0, hi_z);                 // Nop
-        #tCK; nop    (0, hi_z);                 // Nop
-        #tCK; nop    (0, hi_z);                 // Nop
-        #tCK; nop    (0, hi_z);                 // Nop
+        // // Read with auto precharge to bank 0 (non-interrupt)
+        // #tCK; active (0, 0, hi_z);              // Active: Bank = 0, Row = 0
+        // #tCK; NOP    (0, hi_z);                 // Nop
+        // #tCK; NOP    (0, hi_z);                 // Nop
+        // #tCK; read   (0, 1024, hi_z, 0);        // Read  : Bank = 0, Col = 0, Dqm = 0, Auto precharge
+        // #tCK; NOP    (0, hi_z);                 // Nop
+        // #tCK; NOP    (0, hi_z);                 // Nop
+        // #tCK; NOP    (0, hi_z);                 // Nop
+        // #tCK; NOP    (0, hi_z);                 // Nop
 
-        // Read with auto precharge to bank 1 (non-interrupt)
-        #tCK; active (1, 0, hi_z);              // Active: Bank = 1, Row = 0
-        #tCK; nop    (0, hi_z);                 // Nop
-        #tCK; nop    (0, hi_z);                 // Nop
-        #tCK; read   (1, 1024, hi_z, 0);        // Read  : Bank = 1, Col = 0, Dqm = 0, Auto precharge
-        #tCK; nop    (0, hi_z);                 // Nop
-        #tCK; nop    (0, hi_z);                 // Nop
-        #tCK; nop    (0, hi_z);                 // Nop
-        #tCK; nop    (0, hi_z);                 // Nop
+        // // Read with auto precharge to bank 1 (non-interrupt)
+        // #tCK; active (1, 0, hi_z);              // Active: Bank = 1, Row = 0
+        // #tCK; NOP    (0, hi_z);                 // Nop
+        // #tCK; NOP    (0, hi_z);                 // Nop
+        // #tCK; read   (1, 1024, hi_z, 0);        // Read  : Bank = 1, Col = 0, Dqm = 0, Auto precharge
+        // #tCK; NOP    (0, hi_z);                 // Nop
+        // #tCK; NOP    (0, hi_z);                 // Nop
+        // #tCK; NOP    (0, hi_z);                 // Nop
+        // #tCK; NOP    (0, hi_z);                 // Nop
 
-        // Read with auto precharge to bank 2 (non-interrupt)
-        #tCK; active (2, 0, hi_z);              // Active: Bank = 2, Row = 0
-        #tCK; nop    (0, hi_z);                 // Nop
-        #tCK; nop    (0, hi_z);                 // Nop
-        #tCK; read   (2, 1024, hi_z, 0);        // Read  : Bank = 2, Col = 0, Dqm = 0, Auto precharge
-        #tCK; nop    (0, hi_z);                 // Nop
-        #tCK; nop    (0, hi_z);                 // Nop
-        #tCK; nop    (0, hi_z);                 // Nop
-        #tCK; nop    (0, hi_z);                 // Nop
+        // // Read with auto precharge to bank 2 (non-interrupt)
+        // #tCK; active (2, 0, hi_z);              // Active: Bank = 2, Row = 0
+        // #tCK; NOP    (0, hi_z);                 // Nop
+        // #tCK; NOP    (0, hi_z);                 // Nop
+        // #tCK; read   (2, 1024, hi_z, 0);        // Read  : Bank = 2, Col = 0, Dqm = 0, Auto precharge
+        // #tCK; NOP    (0, hi_z);                 // Nop
+        // #tCK; NOP    (0, hi_z);                 // Nop
+        // #tCK; NOP    (0, hi_z);                 // Nop
+        // #tCK; NOP    (0, hi_z);                 // Nop
 
-        // Read with auto precharge to bank 3 (non-interrupt)
-        #tCK; active (3, 0, hi_z);              // Active: Bank = 3, Row = 0
-        #tCK; nop    (0, hi_z);                 // Nop
-        #tCK; nop    (0, hi_z);                 // Nop
-        #tCK; read   (3, 1024, hi_z, 0);        // Read  : Bank = 3, Col = 0, Dqm = 0, Auto precharge
-        #tCK; nop    (0, hi_z);                 // Nop
-        #tCK; nop    (0, hi_z);                 // Nop
-        #tCK; nop    (0, hi_z);                 // Nop
-        #tCK; nop    (0, hi_z);                 // Nop
+        // // Read with auto precharge to bank 3 (non-interrupt)
+        // #tCK; active (3, 0, hi_z);              // Active: Bank = 3, Row = 0
+        // #tCK; NOP    (0, hi_z);                 // Nop
+        // #tCK; NOP    (0, hi_z);                 // Nop
+        // #tCK; read   (3, 1024, hi_z, 0);        // Read  : Bank = 3, Col = 0, Dqm = 0, Auto precharge
+        // #tCK; NOP    (0, hi_z);                 // Nop
+        // #tCK; NOP    (0, hi_z);                 // Nop
+        // #tCK; NOP    (0, hi_z);                 // Nop
+        // #tCK; NOP    (0, hi_z);                 // Nop
 
-        #tCK; nop    (0, hi_z);                 // Nop
-        #tCK; nop    (0, hi_z);                 // Nop
-        #tCK; nop    (0, hi_z);                 // Nop
-        #tCK;
+        // #tCK; NOP    (0, hi_z);                 // Nop
+        // #tCK; NOP    (0, hi_z);                 // Nop
+        // #tCK; NOP    (0, hi_z);                 // Nop
+        // #tCK;
     end
 $stop;
 $finish;
