0f2521617d0f974e2e754c284f12dda498533046
myyerrol <myyerrol@126.com>
2023-10-28 17:13:28 +0800

    feat: add new functions

--- rtl/common/clk.v
+++ rtl/common/clk.v
@@ -2,13 +2,16 @@
 
 module clk #(
     parameter NUM_FREQ_DIV_EVEN = 2,
-    parameter NUM_FREQ_DIV_ODD  = 3
+    parameter NUM_FREQ_DIV_ODD  = 3,
+    parameter NUM_FREQ_MUL      = 2
 ) (
     input  wire i_clk,
     input  wire i_rst_n,
 
     output reg  o_clk_div_even,
-    output wire o_clk_div_odd
+    output wire o_clk_div_odd,
+    output wire o_clk_mul_dly_2x,
+    output reg  o_clk_mul_pll
 );
 
 //-----------------------------------------------------------------------------
@@ -16,7 +19,7 @@ module clk #(
 
 reg [7 : 0] cnt_div_even;
 
-parameter FREQ_REF     = 50_000_000;
+parameter FREQ_REF     = 50_000_000; // 50MHz
 parameter CNT_DIV_EVEN = NUM_FREQ_DIV_EVEN / 2 - 1;
 
 always @(posedge i_clk or negedge i_rst_n) begin
@@ -55,6 +58,8 @@ always @(negedge i_clk or negedge i_rst_n) begin
         else if (cnt_div_odd_pos == NUM_FREQ_DIV_ODD - 2) begin
             clk_div_odd_pos <= 1'b0;
         end
+        else begin
+        end
 
         if (cnt_div_odd_pos == NUM_FREQ_DIV_ODD - 1) begin
             cnt_div_odd_pos <= 8'b0;
@@ -77,6 +82,8 @@ always @(posedge i_clk or negedge i_rst_n) begin
         else if (cnt_div_odd_neg == NUM_FREQ_DIV_ODD - 2) begin
             clk_div_odd_neg <= 1'b0;
         end
+        else begin
+        end
 
         if (cnt_div_odd_neg == NUM_FREQ_DIV_ODD - 1) begin
             cnt_div_odd_neg <= 8'b0;
@@ -88,5 +95,23 @@ always @(posedge i_clk or negedge i_rst_n) begin
 end
 
 assign o_clk_div_odd = clk_div_odd_pos | clk_div_odd_neg;
+
+//-----------------------------------------------------------------------------
+// Frequency Multiplier (Delay way)
+
+parameter CYCLE_REF = 20; // 50MHz
+
+reg clk_mul_dly_2x;
+
+always @(posedge i_clk or negedge i_rst_n) begin
+    if (!i_rst_n) begin
+        clk_mul_dly_2x <= 1'b0;
+    end
+    else begin
+        clk_mul_dly_2x <= #(CYCLE_REF / 2 / 2) ~clk_mul_dly_2x;
+    end
+end
+
+assign o_clk_mul_dly_2x = i_clk ^ clk_mul_dly_2x;
 
 endmodule
--- sim/common/tb_clk.v
+++ sim/common/tb_clk.v
@@ -9,6 +9,7 @@ reg sys_rst_n;
 
 wire sys_clk_div_even;
 wire sys_clk_div_odd;
+wire sys_clk_mul_dly_2x;
 
 initial begin
     sys_clk   = 1'b1;
@@ -20,11 +21,12 @@ end
 always #(CYCLE / 2) sys_clk = ~sys_clk;
 
 clk clk_inst(
-    .i_clk         (sys_clk),
-    .i_rst_n       (sys_rst_n),
+    .i_clk           d(sys_clk),
+    .i_rst_n         (sys_rst_n),
 
-    .o_clk_div_even(sys_clk_div_even),
-    .o_clk_div_odd (sys_clk_div_odd)
+    .o_clk_div_even  (sys_clk_div_even),
+    .o_clk_div_odd   (sys_clk_div_odd),
+    .o_clk_mul_dly_2x(sys_clk_mul_dly_2x)
 );
 
 endmodule
