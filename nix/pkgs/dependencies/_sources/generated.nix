022151aba7e24acb8fc5a936a16e462fbaadd843
Jiuyang Liu <liu@jiuyang.me>
2024-08-29 01:21:12 +0800

    bump chisel and chisel-interface

--- nix/pkgs/dependencies/_sources/generated.nix
+++ nix/pkgs/dependencies/_sources/generated.nix
@@ -3,14 +3,26 @@
 {
   chisel = {
     pname = "chisel";
-    version = "f347914dee389cebbe48ac4ef2e350e5ac1d267a";
+    version = "4c0c194710e75dd022c1c1eefa38734ccd5c6cd6";
     src = fetchFromGitHub {
       owner = "chipsalliance";
       repo = "chisel";
-      rev = "f347914dee389cebbe48ac4ef2e350e5ac1d267a";
+      rev = "4c0c194710e75dd022c1c1eefa38734ccd5c6cd6";
       fetchSubmodules = false;
-      sha256 = "sha256-4sdVOhXt2Y4pmzLpImi+jvpi2exJ5f0vuN+iLnueESk=";
+      sha256 = "sha256-kOI8S9YX7nRsJ1vUPeuXZJfwVuE6hgKjCAJ+HDSkk4Y=";
     };
-    date = "2024-08-15";
+    date = "2024-08-28";
+  };
+  chisel-interface = {
+    pname = "chisel-interface";
+    version = "0d97ca7872f69cfcfb9ae1d72059bd62be606322";
+    src = fetchFromGitHub {
+      owner = "chipsalliance";
+      repo = "chisel-interface";
+      rev = "0d97ca7872f69cfcfb9ae1d72059bd62be606322";
+      fetchSubmodules = false;
+      sha256 = "sha256-R0F6AsxoKyFE334bF567er+InzQQMp9bLTlal/SgVhs=";
+    };
+    date = "2024-05-22";
   };
 }
